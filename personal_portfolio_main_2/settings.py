a7b4ab4eab34cc28a2413954de304b8eadf07d7f
saeed <saeedbagheri75@gmail.com>
2020-04-10 21:36:04 +0000

    Remove old stuff

--- personal_portfolio_main_2/settings.py
+++ personal_portfolio_main_2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tomm=e0#!$gz(o0ldb(=+^0#&%kn&h_v@+5r)ew@y=0xwi6g+k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saeedb73.pythonanywhere.com']
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'ASIA/TEHRAN'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
